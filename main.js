d0185838a070cc2660106a238474594202a16772
dgopinath3693 <dgopinath3693@gmail.com>
2025-04-05 21:28:03 -0500

    setup basic functionality of app

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 400,
+        height: 200,
+        frame: false,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong')
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
